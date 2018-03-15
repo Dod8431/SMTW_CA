f8f63c8dbd43e192d7a93fa2b55654f9034bc045
Dod8431 <32450556+Dod8431@users.noreply.github.com>
2018-03-15 16:20:19 +0100

    Cafaitchier

---
+++ Assets/Script/CameraControllerMiniGame.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControllerMiniGame : MonoBehaviour {
+
+   // public GameObject vea;
+    public float speed;
+    private Vector3 veapos;
+    bool exit = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(exit)
+        {
+            this.transform.position = Vector3.MoveTowards(this.transform.position, veapos, speed * Time.deltaTime);
+            if (this.transform.position == veapos)
+            {
+                exit = false;
+            }
+        }
+	}
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag ("Vea"))
+        {
+            veapos = new Vector3 (other.transform.position.x,2, other.transform.position.z);
+            exit = true;
+        }
+    }
+}
