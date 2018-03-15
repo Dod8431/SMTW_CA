be69a3d20f2112a0497b3462f7678f08e1695207
Axel-Arnaud <arnaudaxrl.pro@gmail.com>
2018-03-15 16:13:45 +0100

    Axel-15-03-merge

---
+++ Assets/Script/Camera_Controller_Gyro.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Controller_Gyro : MonoBehaviour {
+    private Gyroscope gyro;
+    public bool gyroEnabled = true;
+    bool haveGyro;
+    Quaternion rot;
+    GameObject cameraContainer;
+    public float turnSpeed = 4.0f;
+    Vector3 mouseOrigin;
+    bool isRotating;
+
+
+    void Start()
+    {
+
+        cameraContainer = new GameObject("Camera Container");
+        cameraContainer.transform.position = this.transform.position;
+        transform.SetParent(cameraContainer.transform);
+        haveGyro = EnableGyro();
+
+    }
+    private bool EnableGyro()
+    {
+
+        if (SystemInfo.supportsGyroscope)
+        {
+            gyro = Input.gyro;
+            gyro.enabled = true;
+
+            cameraContainer.transform.rotation = Quaternion.Euler(90f, 90f, 0f);
+            rot = new Quaternion(0, 0, 1, 0);
+            return true;
+        }
+        return false;
+    }
+
+    void Update()
+    {
+        if (haveGyro && gyroEnabled)
+        {
+            transform.localRotation = gyro.attitude * rot;
+        }
+        else
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                // Get mouse origin
+                gyroEnabled = false;
+                mouseOrigin = Input.mousePosition;
+                isRotating = true;
+            }
+        }
+
+        if (isRotating)
+        {
+            Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - mouseOrigin);
+
+            transform.RotateAround(transform.position, transform.right, -pos.y * turnSpeed);
+            transform.RotateAround(transform.position, Vector3.up, pos.x * turnSpeed);
+        }
+    }
+
+    public void ButtonGyro()
+    {
+        gyroEnabled = !gyroEnabled;
+    }
+
+}
---
+++ Assets/Script/CheckSwipe.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckSwipe : MonoBehaviour {
+
+    Vector2 fingerDown;
+    Vector2 fingerUp;
+    public bool waitForSwipe = false;
+    public float swipeRange = 20f;
+    Rigidbody vearb;
+    public float speed; 
+
+    public GameObject vea;
+
+    // Use this for initialization
+    void Start () {
+        vearb = vea.GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ////Test
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            vearb.velocity = new Vector3(0, 0, speed);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            vearb.velocity = new Vector3(0, 0, -speed);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            vearb.velocity = new Vector3(-speed, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            vearb.velocity = new Vector3(speed, 0, 0);
+        }
+        ///////////////////////
+
+
+        foreach (Touch touch in Input.touches)
+        {
+            if (touch.phase == TouchPhase.Began)
+            {
+                fingerUp = touch.position;
+                fingerDown = touch.position;
+            }
+
+            //Detects Swipe while finger is still moving
+            if (touch.phase == TouchPhase.Moved)
+            {
+                if (!waitForSwipe)
+                {
+                    fingerDown = touch.position;
+                    checkSwipeDir();
+                }
+            }
+
+            //Detects swipe after finger is released
+            if (touch.phase == TouchPhase.Ended)
+            {
+                fingerDown = touch.position;
+                checkSwipeDir();
+            }
+        }
+    }
+
+    void checkSwipeDir()
+    {
+        //Check if Vertical swipe
+        if (verticalMove() > swipeRange && verticalMove() > horizontalMove())
+        {
+            //Debug.Log("Vertical");
+            if (fingerDown.y - fingerUp.y > 0)//up swipe
+            {
+                OnSwipeUp();
+            }
+            else if (fingerDown.y - fingerUp.y < 0)//Down swipe
+            {
+                OnSwipeDown();
+            }
+            fingerUp = fingerDown;
+        }
+
+        //Check if Horizontal swipe
+        else if (horizontalMove() > swipeRange && horizontalMove() > verticalMove())
+        {
+
+            if (fingerDown.x - fingerUp.x > 0)//Right swipe
+            {
+                OnSwipeRight();
+            }
+            else if (fingerDown.x - fingerUp.x < 0)//Left swipe
+            {
+                OnSwipeLeft();
+            }
+            fingerUp = fingerDown;
+        }
+    }
+
+    float verticalMove()
+    {
+        return Mathf.Abs(fingerDown.y - fingerUp.y);
+    }
+
+    float horizontalMove()
+    {
+        return Mathf.Abs(fingerDown.x - fingerUp.x);
+    }
+
+    void OnSwipeUp()
+    {
+        Debug.Log("Swipe UP");
+        vearb.velocity = new Vector3(0, 0, speed);
+        
+    }
+
+    void OnSwipeDown()
+    {
+        Debug.Log("Swipe Down");
+        vearb.velocity = new Vector3(0, 0, -speed);
+    }
+
+    void OnSwipeLeft()
+    {
+        Debug.Log("Swipe Left");
+        vearb.velocity = new Vector3(-speed, 0, 0);
+    }
+
+    void OnSwipeRight()
+    {
+        Debug.Log("Swipe Right");
+        vearb.velocity = new Vector3(speed, 0, 0);
+    }
+
+}
