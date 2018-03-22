446de376a7949e3b6a37f8d37bc6b518c7e75989
Axel-Arnaud <arnaudaxrl.pro@gmail.com>
2018-03-22 21:16:31 +0100

    Axel-mainscene

--- Assets/Script/PZ1/Mask_Controller.cs
+++ Assets/Script/PZ1/Mask_Controller.cs
@@ -4,9 +4,40 @@ using UnityEngine;
 
 public class Mask_Controller : MonoBehaviour {
 
-    public float speed = 0.1F;
+    public float speed = 0.06f;
+    private bool check = false;
 
-    void Update () {
+    public float boundX = 950;
+    public float boundY = 1700;
+    public float correctionZ;
+
+    void Start()
+    {
+        Screen.orientation = ScreenOrientation.Portrait;
+    }
+
+    void Update ()
+    {
+        transform.position = new Vector3
+        (
+            Mathf.Clamp(transform.position.x, -boundX, boundX),
+            Mathf.Clamp(transform.position.y, -boundY, boundY),
+            correctionZ
+        );
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.position += new Vector3(1, 0, 0) * speed;
+        }
+
+        
+
+        if (Input.touchCount >0 && check == false)
+        {
+            GetComponent<Animator>().Play("TorchOpen");
+            check = true;
+        }
+
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
         {
             Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
